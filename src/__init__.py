2efa5b56cb8be0bfcf93634b5a7fccaaaf000c57
Pierre Tessier <pierre.borie.tessier@gmail.com>
2022-10-06 17:02:49 -0400

    add imports

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+    __init__.py
+    Created by pierre
+    10/6/22 - 1:58 PM
+    Description:
+    Init file
+ """
+from cmu_tools import *
---
+++ src/cmu_tools/__init__.py
@@ -0,0 +1,10 @@
+"""
+    __init__.py
+    Created by pierre
+    10/6/22 - 1:52 PM
+    Description:
+    Init file
+ """
+
+from src.cmu_tools.cmu_linker import CMULinker
+from src.cmu_tools.cmu_dictionary import CMUDictionary
