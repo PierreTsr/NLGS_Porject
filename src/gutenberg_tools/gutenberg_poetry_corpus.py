3e89e6777475d5fd14df58cfb60755ee1ca6873a
Pierre Tessier <pierre.borie.tessier@gmail.com>
2022-10-19 00:33:16 -0400

    add corpus split

--- src/gutenberg_tools/gutenberg_poetry_corpus.py
+++ src/gutenberg_tools/gutenberg_poetry_corpus.py
@@ -7,14 +7,16 @@
  """
 import json
 from pathlib import Path
+from tqdm import tqdm
 
 import pandas as pd
 
 
-def build_corpus(path: Path = Path("data/gutenberg_poetry/gutenberg_poetry_corpus")):
+def build_corpus(path: Path = Path("data/gutenberg_poetry/gutenberg_poetry_corpus")) -> dict[int, list[str]]:
+    print("Loading the Gutenberg Poetry Corpus from disk:")
     with open(path, "r") as file:
         l = []
-        for line in file:
+        for line in tqdm(file, total=3085117):
             l.append(json.loads(line.strip()))
     corpus = {}
     for d in l:
@@ -22,18 +24,37 @@ def build_corpus(path: Path = Path("data/gutenberg_poetry/gutenberg_poetry_corpu
         if gid not in corpus.keys():
             corpus[gid] = []
         corpus[gid].append(line)
-    corpus = {gid: "\n".join(lines) for gid, lines in corpus.items()}
-    corpus = pd.DataFrame.from_dict(corpus, orient="index", columns=["text"])
     return corpus
 
 
-def add_metadata(corpus: pd.DataFrame, path: Path = Path("data/gutenberg_poetry/gutenberg_metadata.json")):
+def add_metadata(corpus: pd.DataFrame,
+                 path: Path = Path("data/gutenberg_poetry/gutenberg_metadata.json")) -> pd.DataFrame:
     metadata = pd.read_json(path)
     corpus = pd.merge(corpus, metadata, how="left", left_index=True, right_on="gd-num-padded").reset_index(drop=True)
     return corpus
 
 
-def load_gutenberg(path: Path = Path("data/gutenberg_poetry")):
+def split(corpus: dict[int, list[str]], n_lines: int = 32, stride: int=16) -> pd.DataFrame:
+    rows = []
+    for gid, lines in corpus.items():
+        for i in range(0,len(lines), stride):
+            end = min(i+n_lines, len(lines))
+            rows.append({
+                "gid": gid,
+                "line": i,
+                "text": "\n".join(lines[i:end])
+            })
+    return pd.DataFrame.from_dict(rows)
+
+
+def load_gutenberg(path: Path = Path("data/gutenberg_poetry"), **kwargs):
     corpus = build_corpus(path / "gutenberg_poetry_corpus")
+    if "n_lines" in kwargs.keys() and "stride" in kwargs.keys():
+        corpus = split(corpus, kwargs["n_lines"], kwargs["stride"])
+    else:
+        corpus = {gid: "\n".join(lines) for gid, lines in corpus.items()}
+        corpus = pd.DataFrame.from_dict(corpus, orient="index", columns=["text"])
+        corpus = corpus.reset_index(names=["gid"])
+        corpus["line"] = 0
     corpus = add_metadata(corpus, path / "gutenberg_metadata.json")
     return corpus
