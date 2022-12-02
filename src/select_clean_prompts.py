99b7af5e1c4d6c6b26522699c4450b844c829e6b
Pierre Tessier <pierre.borie.tessier@gmail.com>
2022-12-01 20:24:31 -0500

    prompts selection feature

--- src/select_clean_prompts.py
+++ src/select_clean_prompts.py
@@ -15,7 +15,9 @@ from transformers import AutoTokenizer
 from src import MeterMetrics, CMUDictionary, CMULinker, PronunciationTokenizer
 
 correct_patterns = [
-    "x/x/x/x/x/"
+    "x/x/x/x/x/",
+    "/xx/x/x/x/",
+    "xx/x/x/x/x/"
 ]
 
 dot_id = 13
@@ -31,12 +33,12 @@ def main(args):
     dataset = load_from_disk(args.dataset)["validation"]
     metric = MeterMetrics(linker, tokenizer_p, patterns=correct_patterns, verbose=False)
 
-    prompt_doc = []
     prompt_pos = []
     for doc_idx, doc in tqdm(enumerate(dataset), total=len(dataset)):
         start = 0
         sentence_beginning = True
         seen_dot = False
+        prompt_pos.append([])
         for idx, token in enumerate(doc["input_ids"]):
             if token == unk_id:
                 continue
@@ -47,27 +49,29 @@ def main(args):
                     candidate = doc["input_ids"][start:idx]
                     _, meter_val, _ = metric.max_damerauLevenshtein(candidate)
                     if meter_val >= args.threshold:
-                        prompt_doc.append(doc_idx)
-                        prompt_pos.append((start, idx))
+                        prompt_pos[-1].append((start, idx))
                 start = idx + 1
                 sentence_beginning = seen_dot
             else:
                 seen_dot = False
 
     prompts = []
-    for doc_idx, (start, end) in tqdm(zip(prompt_doc, prompt_pos), total=len(prompt_doc)):
+    for doc_idx, positions in tqdm(enumerate(prompt_pos), total=len(prompt_pos)):
         doc = dataset[doc_idx]
-        line = doc["input_ids"][start:end]
-        entry = { key: val[start:end] for key, val in doc.items()}
-        # entry["text"] = tokenizer.decode(line)
-        prompts.append(entry)
+        for (start, end) in positions:
+            line = doc["input_ids"][start:end]
+            entry = { key: val[start:end] for key, val in doc.items()}
+            entry["text"] = tokenizer.decode(line)
+            prompts.append(entry)
     prompts = Dataset.from_list(prompts)
+    prompts.save_to_disk(args.dest)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dataset", type=str, help="Path to the dataset to use.")
     parser.add_argument("-t", "--threshold", type=float, default="Threshold value for the meter metric.")
+    parser.add_argument("--dest", type=str, help="Path of the new dataset to save.")
     args = parser.parse_args()
 
     raise SystemExit(main(args))
